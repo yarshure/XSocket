1e18ce687181ab9dd85274b06dac5052d8ddcea8
networkextension <b@gmail.com>
2017-11-22 16:44:13 +0800

    Initial Commit

---
+++ XSocket/XSocket.h
@@ -0,0 +1,19 @@
+//
+//  XSocket.h
+//  XSocket
+//
+//  Created by yarshure on 2017/11/22.
+//  Copyright © 2017年 yarshure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XSocket.
+FOUNDATION_EXPORT double XSocketVersionNumber;
+
+//! Project version string for XSocket.
+FOUNDATION_EXPORT const unsigned char XSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XSocket/PublicHeader.h>
+
+
