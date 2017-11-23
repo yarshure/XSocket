dadd84cd11885ed33be7bf35c9e6bbe972c0e5eb
networkextension <b@gmail.com>
2017-11-23 10:57:21 +0800

    add mac framework

---
+++ XSocketMac/XSocketMac.h
@@ -0,0 +1,19 @@
+//
+//  XSocketMac.h
+//  XSocketMac
+//
+//  Created by yarshure on 2017/11/23.
+//  Copyright © 2017年 yarshure. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for XSocketMac.
+FOUNDATION_EXPORT double XSocketMacVersionNumber;
+
+//! Project version string for XSocketMac.
+FOUNDATION_EXPORT const unsigned char XSocketMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XSocketMac/PublicHeader.h>
+
+
